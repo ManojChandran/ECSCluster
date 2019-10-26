2b00e8c54a1a49f2ec22df8c3aa6b741589c4305
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-26 14:53:49 -0400

    testing

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-#resource "null_resource" "cluster" {
-module "subnet" {
-  source = "./modules/subnet"
-  vpc_id = "${var.vpc_id}"
+resource "null_resource" "cluster" {
+  module "subnet" {
+    source = "./modules/subnet"
+    vpc_id = "${var.vpc_id}"
+  }
 }
